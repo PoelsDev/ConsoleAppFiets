a6c538882f6015027f61edd7cc6076190d966d57
ThibaultP-immalle <31651240+ThibaultP-immalle@users.noreply.github.com>
2018-03-27 13:06:59 +0200

    Program

--- ConsoleAppFiets/Program.cs
+++ ConsoleAppFiets/Program.cs
@@ -2,11 +2,35 @@
 
 namespace ConsoleAppFiets
 {
+    class Fiets
+    {
+        private string kleur;
+        public bool HeeftFietsbel { get; set; } = true;
+
+        public string Info()
+        {
+            return $"Deze fiets is {kleur} en heeft {(HeeftFietsbel ? "een" : "geen")} fietsbel";
+        }
+
+        public Fiets(string kleur)
+        {
+            this.kleur = kleur;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Fiets joskesFiets = new Fiets("groen");
+            Fiets miekesFiets = new Fiets("blauw");
+
+            joskesFiets.HeeftFietsbel = true;
+            miekesFiets.HeeftFietsbel = false;
+
+            Console.WriteLine($"Info over Joske's fiets: {joskesFiets.Info()}");
+            Console.WriteLine($"Info over Mieke's fiets: {miekesFiets.Info()}");
         }
     }
+
 }
